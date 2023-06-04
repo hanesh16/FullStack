c710d54ed8d55121b6a39b8e7e9241557571600e
Hanesh <hanesh@pdx.edu>
2023-06-04 20:19:19 +0100

    Implemented the Delete Client mutation in schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,37 +7,37 @@ const Client = require("../models/Client")
 
 
 const { GraphQLObjectType,
-        GraphQLID,
-        GraphQLString,
-        GraphQLSchema,
-        GraphQLEnumType,
-        GraphQLNonNull,
-        GraphQLList}=require('graphql');
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLEnumType,
+    GraphQLNonNull,
+    GraphQLList } = require('graphql');
 
 
 //Client Type
 
 const ClientType = new GraphQLObjectType({
-    name:'Client',
-    fields:()=>({
-        id:{type:GraphQLID},
-        name:{type:GraphQLString},
-        uname:{type:GraphQLString},
-        email:{type:GraphQLString},
-        phone:{type:GraphQLString}
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        uname: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString }
     })
 })
 
 const ItemType = new GraphQLObjectType({
-    name:'Item',
-    fields:()=>({
-        id:{type:GraphQLID},
-        name:{type:GraphQLString},
-        description:{type:GraphQLString},
-        status:{type:GraphQLString},
-        client:{
-            type:ClientType,
-            resolve(parent,args){
+    name: 'Item',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
                 return Client.findById(parent.clientId)
             }
         },
@@ -46,38 +46,56 @@ const ItemType = new GraphQLObjectType({
 })
 
 const mutation = new GraphQLObjectType({
-    name:'Mutation',
-    fields:{
+    name: 'Mutation',
+    fields: {
         //Add Client
-        addClient:{
-            type:ClientType,
-            args:{
-                name:{
-                    type:GraphQLNonNull(GraphQLString)
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
                 },
-                uname:{
-                    type:GraphQLNonNull(GraphQLString)
+                uname: {
+                    type: GraphQLNonNull(GraphQLString)
                 },
-                email:{
-                    type:GraphQLNonNull(GraphQLString)
+                email: {
+                    type: GraphQLNonNull(GraphQLString)
                 },
-                phone:{
-                    type:GraphQLNonNull(GraphQLString)
+                phone: {
+                    type: GraphQLNonNull(GraphQLString)
                 }
             },
-            resolve(parent,args){
-                const client=new Client({
-                    name:args.name,
-                    uname:args.uname,
-                    email:args.email,
-                    phone:args.phone
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    uname: args.uname,
+                    email: args.email,
+                    phone: args.phone
                 });
                 return client.save();
+            }
+        },
+        // Delete Client
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: {
+                    type: GraphQLNonNull(GraphQLID)
+                }
+            },
+            resolve(parent, args) {
+
+                Item.deleteMany({ clientId: args.id }).then(function () {
+                    console.log("Data deleted"); // Success
+                }).catch(function (error) {
+                    console.log(error); // Failure
+                });
+                return Client.findByIdAndRemove(args.id)
             }
         }
     }
 })
 module.exports = new GraphQLSchema({
-    query:RootQuery,
+    query: RootQuery,
     mutation
 })
